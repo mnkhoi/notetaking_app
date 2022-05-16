7d75183c1b3576c6a6eb7c8430bfc834033d5dc3
Khoi Nguyen <bob.k.nguyen@gmail.com>
2022-05-16 15:34:40 -0400

    edit the css and make add form nice

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,36 +1,44 @@
-import { useState } from "react"
-import { nanoid } from 'nanoid'
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const AddNote = ({ onAdd }) => {
+  const [note, setNote] = useState("");
 
-    const [note, setNote] = useState('')
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if (!note) {
-            alert('Please add text')
-            return
-        }
-
-        const date = new Date()
-
-        onAdd({
-            id: nanoid(),
-            note: note,
-            date: date.toLocaleDateString()
-        })
-
-        setNote('')
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!note) {
+      alert("Please add text");
+      return;
     }
+
+    const date = new Date();
+
+    onAdd({
+      id: nanoid(),
+      note: note,
+      date: date.toLocaleDateString(),
+    });
+
+    setNote("");
+  };
 
   return (
     <div className="form">
-    <form className="addform" onSubmit={onSubmit}>
-        <input type="text" style={{height: 'auto'}} name="text" id="text" placeholder="Add Text" value={note} onChange={(e) => setNote(e.target.value)}/>
-      <div className="bottom">
-        <input type="submit" value="Add Note" className="btn" />
-      </div>
-    </form>
+      <form className="addform" onSubmit={onSubmit}>
+          <textarea
+            type="longInput"
+            style={{ height: "auto" }}
+            name="text"
+            id="text"
+            placeholder="Add Text"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          >
+          </textarea>
+        <div className="bottom">
+          <input type="submit" value="Add Note" className="btn" />
+        </div>
+      </form>
     </div>
   );
 };
