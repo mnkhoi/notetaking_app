73c330c8141e71708f5c1c0e59c998010a6efbfa
Khoi Nguyen <bob.k.nguyen@gmail.com>
2022-05-17 00:24:11 -0400

    works when refreshing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import NoteList from "./components/NoteList";
-import { useState } from 'react'
-import { nanoid } from 'nanoid';
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      note: 'here is some notes',
-      date: '10/20/2020'
-    },
-    {
-      id: nanoid(),
-      note: 'here are some notes',
-      date: '10/21/2020'
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const oldNotes = JSON.parse(localStorage.getItem("app-data"));
+    const name = JSON.parse(localStorage.getItem("bruh"))
+    console.log(oldNotes, name);
+    if (oldNotes) {
+      setNotes(oldNotes);
     }
-  ])
+  }, []);
 
   const onAdd = (e) => {
-    setNotes([...notes, e])
-  }
+    const newNotes = [...notes, e];
+    setNotes(newNotes);
+    localStorage.setItem("app-data", JSON.stringify(newNotes));
+  };
 
   const onDelete = (id) => {
-    setNotes(notes.filter((note) => note.id !== id))
-  }
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes);
+    localStorage.setItem("app-data", JSON.stringify(newNotes));
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        Note Taking App
-      </header>
-      <NoteList notes={ notes } onAdd={ onAdd } onDelete={ onDelete }/>
+      <header className="App-header">Note Taking App</header>
+      <NoteList notes={notes} onAdd={onAdd} onDelete={onDelete} />
     </div>
   );
 }
